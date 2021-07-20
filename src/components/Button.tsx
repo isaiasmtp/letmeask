a2e7bf71c95ba522b5cf3f27da5b09d705a3138a
Isaias Martins <isaias.pontes@ccc.ufcg.edu.br>
2021-07-20 07:43:54 -0300

    Create Room and With firebase authentication

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,9 +5,5 @@ import '../styles/button.scss'
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
 export function Button(props: ButtonProps) {
-  return (
-    <button className="button" {...props}>
-
-    </button>
-  )
+  return ( <button className="button" {...props} />  )
 }
