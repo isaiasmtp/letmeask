086404f6c2e550cefd0e819db3be10e9526d64b6
Isaias Martins <isaias.pontes@ccc.ufcg.edu.br>
2021-10-19 13:08:12 -0300

    Updating

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { FormEvent, useState } from "react"
 import { useParams } from "react-router-dom"
 
@@ -16,41 +17,22 @@ type RoomParams = {
   id: string;
 }
 
-export function Room(){
+export function AdminRoom(){
   const { user } = useAuth()
   const params = useParams<RoomParams>()
   const roomId = params.id
 
   const { title, questions } = useRoom(roomId)
 
-  const [newQuestion, setNewQuestion] = useState('')
-
-  async function handleSendQuestion(event: FormEvent) {
-    event.preventDefault()
-
-    if(newQuestion.trim() === "") return
-    if(!user) throw new Error("You must be logged in")
-
-    const question = {
-      content: newQuestion,
-      author: {
-        name: user.name,
-        avatar: user.avatar
-      },
-      isHighlighted: false,
-      isAnswered: false
-    };
-
-    await database.ref(`rooms/${roomId}/questions`).push(question)
-    setNewQuestion('')
-  }
-
   return(
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="letmeask" />
-          <RoomCode code={roomId} />
+          <div>
+            <RoomCode code={roomId} />
+            <Button isOutlined>Encerrar sala</Button>
+          </div>
         </div>
       </header>
     
@@ -60,24 +42,6 @@ export function Room(){
           { questions.length > 0 &&  <span>{questions.length} pergunta(s)</span>}
          
         </div>
-
-        <form onSubmit={handleSendQuestion}>
-          <textarea 
-            placeholder="O que você quer perguntar?" 
-            onChange={event => setNewQuestion(event.target.value)}
-            value={newQuestion}
-          />
-          <div className="form-footer">
-            {!user
-            ? <span>Para enviar uma pergunta, <button>faça seu login</button></span>
-            : <div className="user-info">
-                <img src={user.avatar} alt={user.name}/>
-                <span>{user.name}</span>
-              </div>
-            }
-            <Button disabled={!user} type="submit">Enviar pergunta</Button>
-          </div>
-        </form>
         <div className="question-list">
           {questions.map(question => {
             return(
